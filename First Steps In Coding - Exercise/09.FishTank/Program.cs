f3d69fd9c9669818a0f19b90448f6b96fdf08c6d
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-12 18:48:14 +0300

    Add files via upload

---
+++ First Steps In Coding - Exercise/09.FishTank/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _09.FishTank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenght = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+            int fishTank = lenght * width * height;
+            double liter = fishTank * 0.001;
+            double space = percent / 100;
+            double litersNeeded = liter * (1 - space);
+            Console.WriteLine(litersNeeded);
+        }
+    }
+}
