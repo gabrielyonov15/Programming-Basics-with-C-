39c377c42faee4d833c2237397f51d408efbfbe9
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-13 18:24:31 +0300

    Add files via upload

---
+++ Conditional Statements Advanced - Exercise/08.OnTimeForTheExam/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace _08.OnTimeForTheExam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hourExam = int.Parse(Console.ReadLine());
+            int minutesEXAM = int.Parse(Console.ReadLine());
+            int hourArrival = int.Parse(Console.ReadLine());
+            int minutesArrival = int.Parse(Console.ReadLine());
+            int totalMinutesExam = hourExam * 60 + minutesEXAM;
+            int totalMinutesArrival = hourArrival * 60 + minutesArrival;
+            int totalMinutesDiferent = totalMinutesExam - totalMinutesArrival;
+            if (totalMinutesDiferent >= 0 && totalMinutesDiferent <= 30)
+            {
+                Console.WriteLine("On time");
+                Console.WriteLine($"{totalMinutesDiferent} minutes before the start");
+            }
+            else if (totalMinutesDiferent > 30)
+            {
+                Console.WriteLine("Early");
+                if (totalMinutesDiferent >= 60)
+                {
+                    int hours = totalMinutesDiferent / 60;
+                    int minutes = totalMinutesDiferent - hours * 60;
+                    if (minutes < 10)
+                    {
+                        Console.WriteLine($"{hours}:0{minutes} hours before the start");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{hours}:{minutes} hours before the start");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"{totalMinutesDiferent} minutes before the start");
+                }
+            }
+            else if (totalMinutesDiferent < 0)
+            {
+                Console.WriteLine("Late");
+                totalMinutesDiferent = -totalMinutesDiferent;
+                if (totalMinutesDiferent >= 60)
+                {
+                    int hours = totalMinutesDiferent / 60;
+                    int minutes = totalMinutesDiferent - hours * 60;
+                    if (minutes < 10)
+                    {
+                        Console.WriteLine($"{hours}:0{minutes} hours after the start");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{hours}:{minutes} hours after the start");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"{totalMinutesDiferent} minutes after the start");
+                }
+            }
+        }
+    }
+}
---
+++ Conditional Statements Advanced - Exercise/09.SkiTrip/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace _09.SkiTrip
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            string roomType = Console.ReadLine();
+            string feedback = Console.ReadLine();
+            double pricePerNight = 0;
+            switch (roomType)
+            {
+                case "room for one person":
+                    pricePerNight = 18.00;
+                    break;
+                case "apartment":
+                    pricePerNight = 25.00;
+                    break;
+                case "president apartment":
+                    pricePerNight = 35.00;
+                    break;
+            }
+            double totalPrice = (days - 1) * pricePerNight;
+            if (days > 15)
+            {
+                switch (roomType)
+                {
+                    case "room for one person":
+                        break;
+                    case "apartment":
+                        totalPrice *= 0.50;
+                        break;
+                    case "president apartment":
+                        totalPrice *= 0.80;
+                        break;
+                }
+            }
+            else if (days >= 10)
+            {
+                switch (roomType)
+                {
+                    case "room for one person":
+                        break;
+                    case "apartment":
+                        totalPrice *= 0.65;
+                        break;
+                    case "president apartment":
+                        totalPrice *= 0.85;
+                        break;
+                }
+            }
+            else
+            {
+                switch (roomType)
+                {
+                    case "room for one person":
+                        break;
+                    case "apartment":
+                        totalPrice *= 0.70;
+                        break;
+                    case "president apartment":
+                        totalPrice *= 0.90;
+                        break;
+                }
+            }
+            if (feedback == "positive")
+            {
+                totalPrice *= 1.25;
+            }
+            else
+            {
+                totalPrice *= 0.90;
+            }
+            Console.WriteLine($"{totalPrice:F2}");
+        }
+    }
+}
