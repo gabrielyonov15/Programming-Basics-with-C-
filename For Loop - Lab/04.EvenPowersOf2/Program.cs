18a571434ca1136dca2088ec52b2da2e2fb3af44
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-13 18:26:19 +0300

    Add files via upload

---
+++ For Loop - Lab/04.EvenPowersOf2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _04.EvenPowersOf2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int number = 1;
+            for (int i = 0; i <= n; i += 2)
+            {
+                double result = Math.Pow(2, i);
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
