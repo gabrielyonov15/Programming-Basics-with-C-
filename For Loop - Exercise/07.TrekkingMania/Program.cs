eec6bc17f7346cda40b0057ba48371670690e248
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-13 18:31:13 +0300

    Add files via upload

---
+++ For Loop - Exercise/07.TrekkingMania/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _07.TrekkingMania
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalGroups = int.Parse(Console.ReadLine());
+            int totalClimbers = 0;
+            int musalaClimbers = 0;
+            int montBlancClimbers = 0;
+            int kilimanjaroClimbers = 0;
+            int k2Climbers = 0;
+            int everestClimbers = 0;
+            for (int i = 0; i < totalGroups; i++)
+            {
+                int groupSize = int.Parse(Console.ReadLine());
+                totalClimbers += groupSize;
+                if (groupSize <= 5)
+                {
+                    musalaClimbers += groupSize;
+                }
+                else if (groupSize <= 12)
+                {
+                    montBlancClimbers += groupSize;
+                }
+                else if (groupSize <= 25)
+                {
+                    kilimanjaroClimbers += groupSize;
+                }
+                else if (groupSize <= 40)
+                {
+                    k2Climbers += groupSize;
+                }
+                else
+                {
+                    everestClimbers += groupSize;
+                }
+            }
+            Console.WriteLine($"{(musalaClimbers / (double)totalClimbers) * 100:F2}%");
+            Console.WriteLine($"{(montBlancClimbers / (double)totalClimbers) * 100:F2}%");
+            Console.WriteLine($"{(kilimanjaroClimbers / (double)totalClimbers) * 100:F2}%");
+            Console.WriteLine($"{(k2Climbers / (double)totalClimbers) * 100:F2}%");
+            Console.WriteLine($"{(everestClimbers / (double)totalClimbers) * 100:F2}%");
+        }
+    }
+}
---
+++ For Loop - Exercise/08.TennisRanklist/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _08.TennisRanklist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tornamentCounts = int.Parse(Console.ReadLine());
+            int startingPoints = int.Parse(Console.ReadLine());
+            int tornamentPointCounts = 0;
+            double turnamentsWom = 0;
+            for (int i = 0; i < tornamentCounts; i++)
+            {
+                string position = Console.ReadLine();
+                if (position == "W")
+                {
+                    tornamentPointCounts += 2000;
+                    turnamentsWom++;
+                }
+                else if (position == "F")
+                {
+                    tornamentPointCounts += 1200;
+                }
+                else if (position == "SF")
+                {
+                    tornamentPointCounts += 720;
+                }
+            }
+            Console.WriteLine($"Final points: {tornamentPointCounts + startingPoints}");
+            Console.WriteLine($"Average points: {tornamentPointCounts / tornamentCounts}");
+            Console.WriteLine($"{turnamentsWom / tornamentCounts * 100:f2}%");
+        }
+    }
+}
