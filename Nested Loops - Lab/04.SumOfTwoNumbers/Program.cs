b444fe0b8aaef1a02d3ee017c875a78ee3c481e5
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-13 19:50:31 +0300

    Add files via upload

---
+++ Nested Loops - Lab/04.SumOfTwoNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _04.SumOfTwoNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            int magicNumber = int.Parse(Console.ReadLine());
+            int counter = 0;
+            bool isFound = false;
+            for (int i = start; i <= end; i++)
+            {
+                for (int j = start; j <= end; j++)
+                {
+                    counter++;
+                    if (i + j == magicNumber)
+                    {
+                        Console.WriteLine($"Combination N:{counter} ({i} + {j} = {magicNumber})");
+                        isFound = true;
+                        break;
+                    }
+                }
+                if (isFound)
+                {
+                    break;
+                }
+            }
+            if (!isFound)
+            {
+                Console.WriteLine($"{counter} combinations - neither equals {magicNumber}");
+            }
+        }
+    }
+}
---
+++ Nested Loops - Lab/05.Travelling/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _05.Travelling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+            while (destination != "End")
+            {
+                double neededMoney = double.Parse(Console.ReadLine());
+                double savedMoney = 0;
+                while (savedMoney < neededMoney)
+                {
+                    double currentMoney = double.Parse(Console.ReadLine());
+                    savedMoney += currentMoney;
+                }
+                Console.WriteLine($"Going to {destination}!");
+                destination = Console.ReadLine();
+            }
+        }
+    }
+}
