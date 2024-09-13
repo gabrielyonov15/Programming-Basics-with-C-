febea35028c7c92bbff17aa3947906cc7452fe30
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-13 18:27:22 +0300

    Add files via upload

---
+++ For Loop - Lab/10.OddEvenSum/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _10.OddEvenSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int evenSum = 0;
+            int oddSum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (i % 2 == 0)
+                {
+                    evenSum += num;
+                }
+                else
+                {
+                    oddSum += num;
+                }
+            }
+            int difference = Math.Abs(evenSum - oddSum);
+            if (evenSum == oddSum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine("Sum = " + evenSum);
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine("Diff = " + difference);
+            }
+        }
+    }
+}
